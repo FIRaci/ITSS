5a17e89cc680db45a13bc3312d55054401d311e5
Liem Thanh <nk.tl.bn.2005@gmail.com>
2026-03-03 23:31:45 +0700

    Nguyen Khac Thanh Liem HW1

---
+++ Homework01/20235764-NguyenKhacThanhLiem/MVCJavaFX/CalculatorControllerFx.java
@@ -0,0 +1,23 @@
+public class CalculatorControllerFx {
+	private CalculatorViewFx theView;
+	private CalculatorModelFx theModel;
+	
+	public CalculatorControllerFx(CalculatorViewFx theView, CalculatorModelFx theModel) {
+		this.theView = theView;
+		this.theModel = theModel;
+		
+		this.theView.getCalculateButton().setOnAction(e -> {
+			try{
+				int firstNumber = theView.getFirstNumber();
+				int secondNumber = theView.getSecondNumber();
+				
+				theModel.addTwoNumbers(firstNumber, secondNumber);
+				theView.setCalcSolution(theModel.getCalculationValue());
+			}
+			catch(NumberFormatException ex){
+				System.out.println(ex);
+				theView.displayErrorMessage("Điền 2 số nguyên vào bruh");
+			}
+		});
+	}
+}
