362750e3d3ff23e0be94a45c5e4c91b2b558a6c7
PHAM THE DAT <dphamthe67@gmail.com>
2026-03-04 13:09:20 +0700

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		String name = JOptionPane.showInputDialog("Hay nhap ten cua ban:");
-		JOptionPane.showMessageDialog(null, "Hello " + name  );
-	}
-}
