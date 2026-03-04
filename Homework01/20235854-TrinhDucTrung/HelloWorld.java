5c01687a2e493d6298ff98448d757460de65f805
trung_175 <trinhtrung171105@gmail.com>
2026-03-04 12:55:56 +0700

    del

--- Homework01/20235854-TrinhDucTrung/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		String name = JOptionPane.showInputDialog("Nhập tên:");
-		JOptionPane.showMessageDialog(null, "Chào bạn " + name + "!");
-	}
-}
