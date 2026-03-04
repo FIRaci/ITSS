67175d85dd3409887a774d2f49a380421307edbf
hoangducanh2005 <Anh.Hd235640@sis.hust.edu.vn>
2026-03-04 07:23:29 +0700

    Update HelloWorld and project config

--- Homework01/20235640-HoangDucAnh/HelloWorld.java
+++ Homework01/20235640-HoangDucAnh/HelloWorld.java
@@ -1,10 +1,10 @@
-import javax.swing.JOptionPane;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        String result;
-        result = JOptionPane.showInputDialog("Hi! What's your name?");
-        JOptionPane.showMessageDialog(null, "Hello " + result + "!");
-        System.exit(0);
-    }
-}
+import javax.swing.JOptionPane; 
+ 
+public class HelloWorld { 
+    public static void main(String[] args) { 
+        String result; 
+        result = JOptionPane.showInputDialog("Hi! What's your name?"); 
+        JOptionPane.showMessageDialog(null, "Hello " + result + "!"); 
+        System.exit(0); 
+    } 
+} 
